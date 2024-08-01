56fea255716dd55cc84a14df1c2a73c1c440f53d
Muhamad Amin Wafi Bin Ahmad Hisamuddin <aminwafi98@gmail.com>
2024-08-01 22:31:19 +0800

    feat backend: update code with process env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,15 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'host.docker.internal',
+      host: process.env.DB_HOSTNAME,
       port: 5432,
-      username: 'postgres',
-      password: 'amin1234',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: '',
       entities: [__dirname, '/**/*.entity{.ts,.js}'],
       synchronize: true
--- server/src/guards/google-oauth/google-oauth.strategy.ts
+++ server/src/guards/google-oauth/google-oauth.strategy.ts
@@ -11,7 +11,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         super({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.CALLBACK_URL,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
             scope: ['profile', 'email'],
         });
     }
