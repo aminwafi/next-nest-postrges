7c106b3fa9d54c44b6fac3e57cbe672c7aa01de5
Muhamad Amin Wafi Bin Ahmad Hisamuddin <aminwafi98@gmail.com>
2024-08-05 02:03:55 +0800

    feat frontend: create email mask/visibility

--- client/src/app/dashboard/Table.tsx
+++ client/src/app/dashboard/Table.tsx
@@ -1,11 +1,25 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { UserTable } from '../../interface/User';
 import styles from './Table.module.css';
-
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Table: React.FC<UserTable> = ({ data }) => {
+  const [ visibleEmails, setVisibleEmails ] = useState<Set<number>>(new Set());
+
+  const toggleEmailVisibility = (userId: number) => {
+    setVisibleEmails(prev => {
+      const newSet = new Set(prev);
+      if (newSet.has(userId)) {
+        newSet.delete(userId);
+      } else {
+        newSet.add(userId);
+      }
+      return newSet;
+    });
+  };
+  
   return (
     <table className={styles.table}>
       <thead>
@@ -21,7 +35,15 @@ const Table: React.FC<UserTable> = ({ data }) => {
         {data.map(user => (
           <tr key={user.id}>
             <td className={styles.td}>{user.id}</td>
-            <td className={styles.td}>{user.email}</td>
+            <td className={styles.td}>
+              {visibleEmails.has(user.id) ? user.email : '******'}
+              <button
+                onClick={() => toggleEmailVisibility(user.id)}
+                className={styles.iconButton}
+              >
+                {visibleEmails.has(user.id) ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </td>
             <td className={styles.td}>{user.first_name}</td>
             <td className={styles.td}>{user.last_name}</td>
             <td className={styles.td}>
