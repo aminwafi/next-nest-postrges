29afb58974bf654ef8d7aedd43d5162e2114662d
Muhamad Amin Wafi Bin Ahmad Hisamuddin <aminwafi98@gmail.com>
2024-08-06 15:16:32 +0800

    feat backend: update product.service.spec with 84% coverage

--- server/src/products/products.service.spec.ts
+++ server/src/products/products.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 
 import { ProductsService } from './products.service';
 import { Product } from './entities/product.entity';
@@ -19,11 +19,13 @@ describe('ProductsService', () => {
         {
           provide: getRepositoryToken(Product),
           useValue: {
-            findOneBy: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
             find: jest.fn(),
+            merge: jest.fn(),
+            findOneBy: jest.fn(),
             findOne: jest.fn(),
             delete: jest.fn(),
-            save: jest.fn()
           },
         }
       ],
@@ -37,46 +39,62 @@ describe('ProductsService', () => {
   //   expect(service).toBeDefined();
   // });
 
-  // describe('create', () => {
-  //   it('should create a product', async () => {
-  //     const createProductDto: CreateProductDto = { productCode: 'T000-create', location: 'East Malaysia', price: 100, productDescription: 'test product: T000' };
-  //     jest.spyOn(repository, 'create').mockResolvedValue(createProductDto);
+  describe('create', () => {
+    it('should create a product', async () => {
+      const createProductDto: CreateProductDto = { productCode: '1000', location: 'West Malaysia', price: 300, productDescription: 'test now' };
+      const result: Product  = { id: 4, ...createProductDto };
+      jest.spyOn(repository, 'create').mockReturnValue({ ...createProductDto, id: undefined } as Product);
+      jest.spyOn(repository, 'save').mockResolvedValue(result);
 
-  //     const result = await service.create(createProductDto);
-  //     expect(result).toEqual(createProductDto);
-  //     expect(repository.create).toHaveBeenCalledWith(createProductDto);
-  //   })
-  // });
+      const createdProduct = await service.create(createProductDto);
+      expect(createdProduct).toEqual(result);
+      expect(repository.save).toHaveBeenCalledWith(createProductDto);
+    })
+  });
 
   describe('findByQuery', () => {
     it('should return product with matching productCode', async () => {
       const result = [{ id: 4, productCode: '1000', location: 'West Malaysia', price: 300, productDescription: 'test now' }];
       jest.spyOn(repository, 'find').mockResolvedValue(result);
 
-      expect(await service.findByQuery('T000-create', 'East Malaysia')).toBe(result);
+      expect(await service.findByQuery('1000', 'West Malaysia')).toBe(result);
     });
   });
 
   describe('updateByProductCode', () => {
-    it('should update product with matching productCode', async () => {
-      const updateProductDto: UpdateProductDto = { location: 'West Malaysia', price: 200 };
-      const result = { id: 4, productCode: '1000', location: 'West Malaysia', price: 300, productDescription: 'test now' };
-      jest.spyOn(repository, 'findOne').mockResolvedValue(result);
-      jest.spyOn(repository, 'save').mockResolvedValue({ ...result, ...updateProductDto })
-      
-      expect(await service.updateByProductCode('1000', updateProductDto)).toEqual({
-        ...result,
+    it('should update product with matching productCode', async () => { 
+      const updateProductDto: UpdateProductDto = { location: 'Eest Malaysia', price: 400 };
+      const currentProduct: Product = {
+        id: 4,
+        productCode: '1000',
+        location: 'West Malaysia',
+        price: 300,
+        productDescription: 'test now'
+      };
+
+      const result: Product = {
+        ...currentProduct,
         ...updateProductDto
-      });
+      }
+
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(currentProduct);
+      jest.spyOn(repository, 'merge').mockImplementation((product, updateDto) => Object.assign(product, updateDto));
+      jest.spyOn(repository, 'save').mockResolvedValue({ ...result, ...updateProductDto });
+      
+      expect(await service.updateByProductCode('1000', updateProductDto)).toEqual(result);
+      expect(repository.findOneBy).toHaveBeenCalledWith({ productCode: '1000' });
+      expect(repository.merge).toHaveBeenCalledWith(currentProduct, updateProductDto);
+      expect(repository.save).toHaveBeenCalledWith(currentProduct);
+
     });
   });
 
   describe('removeByProductCode', () => {
     it('should delete product with matching productCode', async () => {
-      jest.spyOn(repository, 'delete').mockResolvedValue(undefined);
+      jest.spyOn(repository, 'delete').mockResolvedValue({ affected: 1, raw: {} } as DeleteResult);
 
       await expect(service.removeByProductCode('1000')).resolves.toBeUndefined();
-      expect(repository.delete).toHaveBeenCalledWith('1000');
+      expect(repository.delete).toHaveBeenCalledWith({'productCode': '1000'});
     });
   });
 });
